1a6e3693b1aea8b532bb4a65ffbe90803b1db568
chazcurlee <chaz.curlee@gmail.com>
2022-03-24 17:04:01 -0400

    updated middleware

--- server/models/prize.js
+++ server/models/prize.js
@@ -3,9 +3,10 @@ const { Schema } = require('mongoose')
 const Prize = new Schema(
     {
         // Id:---??
-        cost: { type: Integer, required: true},
+        cost: { type: Number, required: true},
         name: { type: String, required: true},
-        img: { type: String, required: true}
+        img: { type: String, required: true},
+        bought: { type: Boolean, required: true}
     },
     {timestamps: true}
 )
--- server/seed/games.js
+++ server/seed/games.js
@@ -1,5 +1,5 @@
 const db = require('../db')
-const { Game } = require('.../models')
+const { Game } = require('../models')
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
