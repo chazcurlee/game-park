9ec5680361161703d14f083b733082e8b6f5611c
chazcurlee <chaz.curlee@gmail.com>
2022-03-24 09:59:44 -0400

    trying to make it work

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -1,1 +1,18 @@
 import { useEffect, useState } from 'react'
+import Nav from '../src/components/Nav'
+
+
+return (
+
+
+    <div>
+        
+        <h1> Best Online Theme Park Ever!!1!!</h1>
+        <Nav />
+
+
+
+    </div>
+
+
+)
