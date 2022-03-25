a9568ef20bb2ca38e56b1c08f3ad4e85febb51f1
chazcurlee <chaz.curlee@gmail.com>
2022-03-25 12:09:56 -0400

    testtest

--- client/src/components/RewardsGet.jsx
+++ client/src/components/RewardsGet.jsx
@@ -64,6 +64,39 @@ const RewardsGet = () => {
             <img src="https://i.imgur.com/GEwjEYH.png" alt="Teddy Bear" class="prize"/>
             <h1>React is dumb</h1>
             {/* <p>{prizes}</p> */}
+            <p>{prizes}</p>
+        <div>
+            <ul class="thePrizes">
+                <li>
+                    <img src='https://i.imgur.com/F84yv0K.png' alt="Rasta Banana" class="prize"/>
+                    <p>20 tickets</p>
+                </li>
+                <li>
+                    <img src="https://i.imgur.com/GEwjEYH.png" alt="Teddy Bear" class="prize"/>
+                    <p>10 tickets</p>
+                </li>
+                <li>
+                    <img src="https://i.imgur.com/V5Vw9Ik.png" alt="Car" class="prize" id="car"/>
+                    <p>100 tickets</p>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <ul class="thePrizes">
+                <li>
+                    <img src="https://i.imgur.com/KO6oilE.png" alt="Chicken" class="prize"/>
+                    <p>75 tickets</p>
+                </li>
+                <li>
+                    <img src="https://i.imgur.com/olsQrkE.png" alt="Goldfish" class="prize"/>
+                    <p>300 tickets</p>
+                </li>
+                <li>
+                    <img src="https://i.imgur.com/PzprEHO.png" alt="Watch" class="prize" id="watch"/>
+                    <p>35 tickets</p>
+                </li>
+            </ul>
+        </div>
         </div>
     )
 
