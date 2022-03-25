0b0087ef5e2fe208e25d68e05bbd746fbc1af4a5
Kat Imperial <imperialkatherine@gmail.com>
2022-03-25 01:23:18 -0400

    removed react is dumb lol

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,8 +6,6 @@ const Home = () => {
     return (
         <div>
             <img class="welcome" src='https://i.imgur.com/bzuv2Dg.png' alt='Welcome to Game Park'/>
-
-            <h1>React is dumb</h1>
         </div>
     )
 
